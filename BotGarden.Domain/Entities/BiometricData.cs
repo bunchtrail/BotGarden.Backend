6e507a6af85a68e2a971dabfb4623d22ac970894
nikita <loleenko@vk.com>
2024-07-25 17:19:27 +0300

    Remove InventoryNumber property from data classes
    The `InventoryNumber` property, previously commented as `// Инв_номер`, was removed from both the `BiometricData` and `PhenologicalData` classes.

--- BotGarden.Domain/Entities/BiometricData.cs
+++ BotGarden.Domain/Entities/BiometricData.cs
@@ -4,7 +4,6 @@
     {
         public int ID { get; set; }
         public string InventoryNumber { get; set; }  // Инв_номер
-        public Plant Plant { get; set; }
         public string Indicator { get; set; }  // Показатель
         public double Value { get; set; }  // Значение
         public DateTime MeasurementDate { get; set; }  // Дата_измерения
--- BotGarden.Domain/Entities/PhenologicalData.cs
+++ BotGarden.Domain/Entities/PhenologicalData.cs
@@ -4,7 +4,6 @@
     {
         public int ID { get; set; }
         public string InventoryNumber { get; set; }  // Инв_номер
-        public Plant Plant { get; set; }
         public string ObservationYear { get; set; }  // Год_наблюдений
         public DateTime Phenophase { get; set; }  // Фенофаза
     }
