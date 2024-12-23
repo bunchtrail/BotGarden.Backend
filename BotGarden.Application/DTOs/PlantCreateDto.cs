267ceb71e236df4c1d58bf647f2263fa0aa6014d
Nikita Lolenko <loleenko@vk.com>
2024-12-23 22:57:32 +0300

    Enhance PlantCreateDto with additional properties and validation attributes for improved data integrity and structure.

--- BotGarden.Application/DTOs/PlantCreateDto.cs
+++ BotGarden.Application/DTOs/PlantCreateDto.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace BotGarden.Application.DTOs
 {
@@ -12,32 +9,85 @@ namespace BotGarden.Application.DTOs
         public int? BiometricId { get; set; }
         public int? SectorId { get; set; }
         public int? GenusId { get; set; }
+        
+        [MaxLength(100)]
         public string? InventorNumber { get; set; }
+        
+        [MaxLength(100)]
         public string? Species { get; set; }
+        
+        [MaxLength(100)]
         public string? Variety { get; set; }
+        
+        [MaxLength(100)]
         public string? Form { get; set; }
+        
+        [MaxLength(100)]
         public string? Determined { get; set; }
+        
+        [MaxLength(50)]
         public string? YearOfObs { get; set; }
+        
+        [MaxLength(50)]
         public string? PhenophaseDate { get; set; }
+        
+        [MaxLength(50)]
         public string? Year { get; set; }
+        
+        [MaxLength(100)]
         public string? MeasurementType { get; set; }
+        
+        [MaxLength(100)]
         public string? Value { get; set; }
+        
+        [MaxLength(50)]
         public string? DateOfPlanting { get; set; }
+        
+        [MaxLength(100)]
         public string? ProtectionStatus { get; set; }
+        
+        [MaxLength(100)]
         public string? FilledOut { get; set; }
+        
+        [MaxLength(100)]
         public string? HerbariumDuplicate { get; set; }
+        
+        [MaxLength(500)]
         public string? Synonyms { get; set; }
+        
+        [MaxLength(500)]
         public string? PlantOrigin { get; set; }
+        
+        [MaxLength(500)]
         public string? NaturalHabitat { get; set; }
+        
+        [MaxLength(1000)]
         public string? EcologyBiology { get; set; }
+        
+        [MaxLength(1000)]
         public string? EconomicUse { get; set; }
+        
+        [MaxLength(50)]
         public string? Latitude { get; set; }
+        
+        [MaxLength(50)]
         public string? Longitude { get; set; }
+        
+        [MaxLength(100)]
         public string? Originator { get; set; }
+        
+        [MaxLength(50)]
         public string? Date { get; set; }
+        
+        [MaxLength(100)]
         public string? Country { get; set; }
+        
+        [MaxLength(500)]
         public string? ImagePath { get; set; }
+        
         public bool HerbariumPresence { get; set; }
+        
+        [MaxLength(1000)]
         public string? Note { get; set; }
     }
 }
